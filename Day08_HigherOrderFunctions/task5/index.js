32294a1712e6eb84e7951abf25c6c631e8c4fce0
luka2477 <luke.da.duke@hotmail.com>
2022-09-26 10:52:44 +0200

    Parameter composition in Day08/task5/index.js

--- Day08_HigherOrderFunctions/task5/index.js
+++ Day08_HigherOrderFunctions/task5/index.js
@@ -31,3 +31,21 @@ const compose3 = (fs) => {
 };
 
 console.log(compose3([(x) => x + 2, (x) => x + 4, (x) => x + 8, () => 16]));
+
+const compose3other = (...fs) => {
+  let lastf;
+  for (let i = fs.length - 1; i >= 0; i--) {
+    if (!lastf) lastf = fs[i]();
+    else lastf = fs[i](lastf);
+  }
+  return lastf;
+};
+
+console.log(
+  compose3other(
+    (x) => x + 2,
+    (x) => x + 4,
+    (x) => x + 8,
+    () => 16
+  )
+);
