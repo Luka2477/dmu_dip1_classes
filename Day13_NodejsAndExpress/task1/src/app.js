16ad805de0d7aa50a877e8dfe9e167fbf15e7b75
luka2477 <luke.da.duke@hotmail.com>
2022-11-01 11:31:44 +0100

    Now using morgan

--- Day13_NodejsAndExpress/task1/src/app.js
+++ Day13_NodejsAndExpress/task1/src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
+import morgan from "morgan";
 
 import fileRouter from "./routes/fileRoutes.js";
 
 const app = express();
+
+if (process.env.NODE_ENV !== "production") app.use(morgan("dev"));
 
 app.use("/api/v1/files", fileRouter);
 
