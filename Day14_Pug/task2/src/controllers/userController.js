de5a40dc5bc0f394ed0ab4c6cfa019daac619292
luka2477 <luke.da.duke@hotmail.com>
2022-11-11 09:33:54 +0100

    Finish task2

--- Day14_Pug/task2/src/controllers/userController.js
+++ Day14_Pug/task2/src/controllers/userController.js
@@ -1,8 +1,22 @@
 const get = async (req, res) => {
   let testUser = {
-    username: "luka2477",
-    age: 22,
-    created_at: new Date().toDateString(),
+    users: [
+      {
+        username: "luka2477",
+        age: 22,
+        created_at: new Date().toDateString(),
+      },
+      {
+        username: "mike123123",
+        age: 26,
+        created_at: new Date().toDateString(),
+      },
+      {
+        username: "esbena91239",
+        age: 21,
+        created_at: new Date().toDateString(),
+      },
+    ],
   };
 
   res.render("users", testUser);
